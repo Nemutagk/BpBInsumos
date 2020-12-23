273d1dbdac434cab6778005cf17e22e29c21db15
Jose Alberto Cabrera <nemutagk@gmail.com>
2020-12-23 12:20:50 -0600

    En los helper ahora se verifica si ya existe la función antes de intentar definirla

--- src/Helper/Functions.php
+++ src/Helper/Functions.php
@@ -1,178 +1,200 @@
 <?php
-function getBearerToken($request) {
-	return strpos($request->header('Authorization'), 'Bearer') !== false ? substr($request->header('Authorization'), 7) : $request->header('Authorization');
-}
-
-function getNameClass($class) {
-	$partes = explode('\\', get_class($class));
-	return array_pop($partes);
-}
-
-function exception_error($e) {
-	$class = explode('\\',get_class($e));
-
-	$payload = [
-	 	'error'=>$e->getMessage()
-	 	,'file'=>$e->getFile()
-	 	,'line'=>$e->getLine()
-	 	,'code'=>$e->getCode()
-	 	,'trace' => $e->getTraceAsString()
-	 ];
-
-	 if (method_exists($e, 'getResponse'))
-	 	$payload['http_response'] = $e->getResponse();
-
-	 if (method_exists($e, 'getHttpCode'))
-	 	$payload['http_code_response'] = $e->getHttpCode();
-
-	 \Log::error('Error en sistema ('.array_pop($class).'): ',$payload);
-}
-
-function return_exception_error($e, $code=500) {
-	Log::info('return_exception_error');
-
-	$limitTrace = env('ERROR_TRACE_LIMIT',20);
-	$trace = [];
-
-	for($i=0; $i<$limitTrace; $i++) {
-		$trace[] = $e->getTrace()[$i];
+if (!function_exists('getBearerToken')) {
+	function getBearerToken($request) {
+		return strpos($request->header('Authorization'), 'Bearer') !== false ? substr($request->header('Authorization'), 7) : $request->header('Authorization');
 	}
-
-	$payload = [
-		'success' => false
-		,'message' => 'Error al procesar el request'
-		,'error' => $e->getMessage()
-		,'trace' => $trace
-	];
-
-	if (env('APP_ENV') == 'production') {
-		unset($payload['trace']);
-
-		if (strpost($payload['error'], 'SQLSTATE') !== false)
-			$payload['error'] = 'database error';
-	}
-
-	return response($payload, $code);
 }
 
-function arrayKeyLowerToUpper($arr) {
-	$newArr = [];
-	if (range(0, count($arr)-1) !== $arr) {
-		foreach($arr as $key => $val) {
-			if (!is_array($val))
-				$newArr[strtoupper($key)] = $val;
-			else
-				$newArr[strtoupper($key)] = arrayKeyLowerToUpper($val);
+if (!function_exists('getNameClass')) {
+	function getNameClass($class) {
+		$partes = explode('\\', get_class($class));
+		return array_pop($partes);
+	}
+}
+
+if (!function_exists('exception_error')) {
+	function exception_error($e) {
+		$class = explode('\\',get_class($e));
+
+		$payload = [
+		 	'error'=>$e->getMessage()
+		 	,'file'=>$e->getFile()
+		 	,'line'=>$e->getLine()
+		 	,'code'=>$e->getCode()
+		 	,'trace' => $e->getTraceAsString()
+		 ];
+
+		 if (method_exists($e, 'getResponse'))
+		 	$payload['http_response'] = $e->getResponse();
+
+		 if (method_exists($e, 'getHttpCode'))
+		 	$payload['http_code_response'] = $e->getHttpCode();
+
+		 \Log::error('Error en sistema ('.array_pop($class).'): ',$payload);
+	}
+}
+
+if (!function_exists('return_exception_error')) {
+	function return_exception_error($e, $code=500) {
+		Log::info('return_exception_error');
+
+		$limitTrace = env('ERROR_TRACE_LIMIT',20);
+		$trace = [];
+
+		for($i=0; $i<$limitTrace; $i++) {
+			$trace[] = $e->getTrace()[$i];
 		}
-	}else {
-		foreach($arr as $item) {
-			if (!is_array($item))
-				$newArr[] = $item;
-			else
-				$newArr[] = arrayKeyLowerToUpper($item);
-		}
-	}
 
-	return $newArr;
+		$payload = [
+			'success' => false
+			,'message' => 'Error al procesar el request'
+			,'error' => $e->getMessage()
+			,'trace' => $trace
+		];
+
+		if (env('APP_ENV') == 'production') {
+			unset($payload['trace']);
+
+			if (strpost($payload['error'], 'SQLSTATE') !== false)
+				$payload['error'] = 'database error';
+		}
+
+		return response($payload, $code);
+	}
 }
 
-function arrayKeyLower($arr) {
-	$tmpArray = [];
-	foreach($arr as $key => $value) {
-		if (!is_array($value))
-			$tmpArray[lcfirst($key)] = $value;
-		else if (array_keys($value) === range(0, count($value)-1)) {
-			$tmpValue = [];
-			foreach($value as $subArray) {
-				$tmpValue[] = $this->toLower($subArray);
+if (!function_exists('arrayKeyLowerToUpper')) {
+	function arrayKeyLowerToUpper($arr) {
+		$newArr = [];
+		if (range(0, count($arr)-1) !== $arr) {
+			foreach($arr as $key => $val) {
+				if (!is_array($val))
+					$newArr[strtoupper($key)] = $val;
+				else
+					$newArr[strtoupper($key)] = arrayKeyLowerToUpper($val);
 			}
-			$tmpArray[lcfirst($key)] = $tmpValue;
 		}else {
-			$tmpArray[lcfirst($key)] = $this->toLower($value);
-		}
-	}
-
-	return $tmpArray;
-}
-
-function arrayReMap($arr, $map) {
-	$newArr = [];
-	if (range(0, count($arr)-1) !== $arr) {
-		foreach($arr as $key => $val) {
-			if (!is_array($val)) {
-				if (isset($map[$key]))
-					$newArr[$map[$key]] = $val;
+			foreach($arr as $item) {
+				if (!is_array($item))
+					$newArr[] = $item;
 				else
-					$newArr[$key] = $val;
-			}else {
-				if (isset($map[$key]))
-					$newArr[$map[$key]] = arrayReMap($val,$map);
-				else
-					$newArr[$key] = arrayReMap($val,$map);
+					$newArr[] = arrayKeyLowerToUpper($item);
 			}
 		}
-	}else {
-		foreach($arr as $val) {
-			if (!is_array($val))
-				$newArr[] = $val;
-			else
-				$newArr[] = arrayReMap($val, $map);
-		}
+
+		return $newArr;
 	}
-
-	return $newArr;
 }
 
-function str_rand($length=8, $alpha=true) {
-	if ($alpha)
-		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	else
-		$characters = '0123456789';
+if (!function_exists('arrayKeyLower')) {
+	function arrayKeyLower($arr) {
+		$tmpArray = [];
+		foreach($arr as $key => $value) {
+			if (!is_array($value))
+				$tmpArray[lcfirst($key)] = $value;
+			else if (array_keys($value) === range(0, count($value)-1)) {
+				$tmpValue = [];
+				foreach($value as $subArray) {
+					$tmpValue[] = $this->toLower($subArray);
+				}
+				$tmpArray[lcfirst($key)] = $tmpValue;
+			}else {
+				$tmpArray[lcfirst($key)] = $this->toLower($value);
+			}
+		}
 
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
+		return $tmpArray;
+	}
 }
 
-function startProcess() {
-	return \Carbon\Carbon::now();
-}
-
-function endProcess(\Carbon\Carbon $start) {
-	return $start->diffInSeconds(\Carbon\Carbon::now());
-}
-
-function data_set_utf8($data) {
-	if (is_array($data)) {
-		foreach($data as $key => $value) {
-			if (is_string($value)) {
-				if (mb_detect_encoding($value, 'UTF-8', true)) {
-					$data[$key] = $value;
+if (!function_exists('arrayReMap')) {
+	function arrayReMap($arr, $map) {
+		$newArr = [];
+		if (range(0, count($arr)-1) !== $arr) {
+			foreach($arr as $key => $val) {
+				if (!is_array($val)) {
+					if (isset($map[$key]))
+						$newArr[$map[$key]] = $val;
+					else
+						$newArr[$key] = $val;
 				}else {
-					$data[$key] = utf8_encode($value);
+					if (isset($map[$key]))
+						$newArr[$map[$key]] = arrayReMap($val,$map);
+					else
+						$newArr[$key] = arrayReMap($val,$map);
 				}
-			}else if (is_array($value) || is_object($value)) {
-				$data[$key] = data_set_utf8($value);
-			}else {
-				$data[$key] = $value;
+			}
+		}else {
+			foreach($arr as $val) {
+				if (!is_array($val))
+					$newArr[] = $val;
+				else
+					$newArr[] = arrayReMap($val, $map);
 			}
 		}
-	}else {
-		$keys = array_keys(get_object_vars($data));
 
-		foreach($keys as $key) {
-			if (is_string($data->$key)) {
-				if (!mb_detect_encoding($data->$key, 'UTF-8', true)) {
-					$data->$key = utf8_encode($data->$key);
-				}
-			}else if (is_array($data->$key) || is_object($data->$key)) {
-				$data->$key = data_set_utf8($data->$key);
-			}
-		}
+		return $newArr;
 	}
+}
 
-	return $data;
+if (!function_exists('str_rand')) {
+	function str_rand($length=8, $alpha=true) {
+		if ($alpha)
+			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		else
+			$characters = '0123456789';
+
+	    $charactersLength = strlen($characters);
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	    }
+	    return $randomString;
+	}
+}
+
+if (!function_exists('startProcess')) {
+	function startProcess() {
+		return \Carbon\Carbon::now();
+	}
+}
+
+if (!function_exists('endProcess')) {
+	function endProcess(\Carbon\Carbon $start) {
+		return $start->diffInSeconds(\Carbon\Carbon::now());
+	}
+}
+
+if (!function_exists('data_set_utf8')) {
+	function data_set_utf8($data) {
+		if (is_array($data)) {
+			foreach($data as $key => $value) {
+				if (is_string($value)) {
+					if (mb_detect_encoding($value, 'UTF-8', true)) {
+						$data[$key] = $value;
+					}else {
+						$data[$key] = utf8_encode($value);
+					}
+				}else if (is_array($value) || is_object($value)) {
+					$data[$key] = data_set_utf8($value);
+				}else {
+					$data[$key] = $value;
+				}
+			}
+		}else {
+			$keys = array_keys(get_object_vars($data));
+
+			foreach($keys as $key) {
+				if (is_string($data->$key)) {
+					if (!mb_detect_encoding($data->$key, 'UTF-8', true)) {
+						$data->$key = utf8_encode($data->$key);
+					}
+				}else if (is_array($data->$key) || is_object($data->$key)) {
+					$data->$key = data_set_utf8($data->$key);
+				}
+			}
+		}
+
+		return $data;
+	}
 }
