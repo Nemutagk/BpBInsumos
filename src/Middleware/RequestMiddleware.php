4187baca12c64b59de255d5d2be3e882a1cb1539
Jose Cabrera <nemutagk@gmail.com>
2021-07-21 16:26:16 -0500

    Se cambia el error cuando una petición no llega desde un dominio de bpb

--- src/Middleware/RequestMiddleware.php
+++ src/Middleware/RequestMiddleware.php
@@ -43,7 +43,7 @@ class RequestMiddleware
 	        $access->save();
 
 	        if (strpos($request->fullUrl(), 'bienparabien') === false && strpos($request->fullUrl(), 'bpb') === false)
-				return response()->json(['message'=>'Acceso no autorizado'], 401);
+				return response(null, 403);
 		}
 
 		return $next($request);
